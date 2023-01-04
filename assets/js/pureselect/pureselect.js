90c1cf28bbc58adbf1105c1bf7163aa3a5622dcb
developer-kawsar <kawsar.qtec@gmail.com>
2023-01-04 17:08:40 +0600

    homepage edited

---
+++ assets/js/pureselect/pureselect.js
@@ -0,0 +1,46 @@
+// console.log(222);
+// const myOptions = [
+//     {
+//       label: "New York",
+//       value: "NY",
+//     },
+//     {
+//       label: "Washington",
+//       value: "WA",
+//     },
+//     {
+//       label: "California",
+//       value: "CA",
+//     },
+//     {
+//       label: "New Jersey",
+//       value: "NJ",
+//     },
+//     {
+//       label: "North Carolina",
+//       value: "NC",
+//     },
+//   ]
+//   var instance = new SelectPure(".example", {
+//     options: myOptions,
+//     placeholder: false,
+//     multiple: true,
+//     autocomplete: true,
+//     value: ["NY", "CA"],
+//     classNames: {
+//         select: "select-pure__select",
+//         dropdownShown: "select-pure__select--opened",
+//         multiselect: "select-pure__select--multiple",
+//         label: "select-pure__label",
+//         placeholder: "select-pure__placeholder",
+//         dropdown: "select-pure__options",
+//         option: "select-pure__option",
+//         autocompleteInput: "select-pure__autocomplete",
+//         selectedLabel: "select-pure__selected-label",
+//         selectedOption: "select-pure__option--selected",
+//         placeholderHidden: "select-pure__placeholder--hidden",
+//         optionHidden: "select-pure__option--hidden",
+//       },
+
+
+// });
