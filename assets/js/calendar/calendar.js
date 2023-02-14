1485b332c9fa11f7811634d1e74f1c12e23b8b3b
it27427 <istiak.tushar.qtec@gmail.com>
2023-02-14 17:14:15 +0600

    calendar button

--- assets/js/calendar/calendar.js
+++ assets/js/calendar/calendar.js
@@ -5,7 +5,22 @@ document.addEventListener('DOMContentLoaded', function() {
     headerToolbar: {
       left: 'prev,next today',
       center: 'title',
-      right: 'timeGridDay,timeGridWeek,dayGridMonth,dayGridYear'
+      right: 'timeGridDay,timeGridWeek,dayGridMonth,dayGridYear,addtask'
+    },
+    selectable: true,
+    customButtons: {
+      addtask: {
+        text: 'Add Task',
+        click: function() {
+          alert('clicked custom button 1!');
+        }
+      },
+      // custom2: {
+      //   text: 'custom 2',
+      //   click: function() {
+      //     alert('clicked custom button 2!');
+      //   }
+      // }
     }
   });
   
