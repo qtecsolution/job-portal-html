19bea64ae197639b3fca7acf73f41f5644ee935e
it27427 <istiak.tushar.qtec@gmail.com>
2023-02-19 10:12:00 +0600

    inbox page removed

--- assets/js/calendar/calendar.js
+++ assets/js/calendar/calendar.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   let calendarEl = document.getElementById('calendar');
   let calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth',
+    initialView: 'timeGridDay',
     headerToolbar: {
       left: 'prev,next',
       center: 'title',
