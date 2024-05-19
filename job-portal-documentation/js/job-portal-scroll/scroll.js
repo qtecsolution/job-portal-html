061aab55d7629bbf0074b75469a4cc85d669c3df
Jahir <supersimple.jahir@gmail.com>
2024-05-19 18:35:14 +0600

    [Done] issue fixed

--- job-portal-documentation/js/job-portal-scroll/scroll.js
+++ job-portal-documentation/js/job-portal-scroll/scroll.js
@@ -33,10 +33,9 @@ $(".sidebar-dropdown > a").click(function () {
   }
 });
 
-$("#close-sidebar").click(function () {
+$(".close-sidebar").click(function () {
   $(".page-wrapper").removeClass("toggled");
 });
 $("#show-sidebar").click(function () {
   $(".page-wrapper").addClass("toggled");
 });
-
