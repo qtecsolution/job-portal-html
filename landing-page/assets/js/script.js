ba507cb56e9b9b158d8962d67d2fa72cff3b4aae
jahir <supersimple.jahir@gmail.com>
2024-10-03 14:27:02 +0600

    [Done] landing page separate file

---
+++ landing-page/assets/js/script.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".testimonialSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+  },
+});
